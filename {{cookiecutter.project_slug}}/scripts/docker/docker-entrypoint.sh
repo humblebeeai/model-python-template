be76ec18391db432eab83adf765f6eb76223043f
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-07-22 18:45:40 +0900

    script: refactor docker-entrypoint.sh to use variables for sudo and gosu commands

--- {{cookiecutter.project_slug}}/scripts/docker/docker-entrypoint.sh
+++ {{cookiecutter.project_slug}}/scripts/docker/docker-entrypoint.sh
@@ -4,6 +4,16 @@ set -euo pipefail
 
 echo "[INFO]: Running '${PROJECT_SLUG}' docker-entrypoint.sh..."
 
+_SUDO="sudo"
+_GOSU=""
+if [ "$(id -u)" -eq 0 ]; then
+	_SUDO=""
+	_GOSU="gosu ${USER}:${GROUP}"
+elif ! command -v sudo >/dev/null 2>&1; then
+	echo "[ERROR]: 'sudo' is required when not running as root!" >&2
+	exit 1
+fi
+
 _run()
 {
 	if [ -n "${JUPYTERLAB_TOKEN:-}" ]; then
@@ -11,7 +21,7 @@ _run()
 	fi
 
 	echo "[INFO]: Starting Jupyter Lab..."
-	exec gosu "${USER}:${GROUP}" jupyter lab --port="${JUPYTERLAB_PORT:-8888}" || exit 2
+	exec ${_GOSU} jupyter lab --port="${JUPYTERLAB_PORT:-8888}" || exit 2
 	exit 0
 }
 
@@ -19,6 +29,7 @@ _run()
 main()
 {
 	umask 0002 || exit 2
+
 	find "${WORKSPACES_DIR}" \
 		\( \
 			-type d -name ".git" -o \
@@ -29,7 +40,7 @@ main()
 			-type d -name "volumes" -o \
 			-type l -name ".env" \
 		\) -prune -o -print0 | \
-			xargs -0 chown -c "${USER}:${GROUP}" || exit 2
+			xargs -0 ${_SUDO} chown -c "${USER}:${GROUP}" || exit 2
 
 	# find "${PROJECT_DIR}" \
 	# 	\( \
@@ -41,7 +52,7 @@ main()
 	# 		-type d -name "modules" -o \
 	# 		-type d -name "volumes" \
 	# 	\) -prune -o -type d -exec \
-	# 		chmod 775 {} + || exit 2
+	# 		${_SUDO} chmod 775 {} + || exit 2
 
 	# find "${PROJECT_DIR}" \
 	# 	\( \
@@ -55,7 +66,7 @@ main()
 	# 		-type d -name "examples" -o \
 	# 		-type l -name ".env" \
 	# 	\) -prune -o -type f -exec \
-	# 		chmod 664 {} + || exit 2
+	# 		${_SUDO} chmod 664 {} + || exit 2
 
 	# find "${PROJECT_DIR}" \
 	# 	\( \
@@ -67,9 +78,9 @@ main()
 	# 		-type d -name "modules" -o \
 	# 		-type d -name "volumes" \
 	# 	\) -prune -o -type d -exec \
-	# 		chmod ug+s {} + || exit 2
+	# 		${_SUDO} chmod ug+s {} + || exit 2
 
-	gosu "${USER}:${GROUP}" jupyter labextension disable "@jupyterlab/apputils-extension:announcements" || exit 2
+	${_GOSU} jupyter labextension disable "@jupyterlab/apputils-extension:announcements" || exit 2
 	/usr/sbin/sshd -p "${SSH_PORT:-22}" || exit 2
 	# echo "${USER} ALL=(ALL) ALL" | tee -a "/etc/sudoers.d/${USER}" > /dev/null || exit 2
 	echo ""
@@ -83,10 +94,10 @@ main()
 			shift
 			if [ -z "${*:-}" ]; then
 				echo "[INFO]: Starting bash..."
-				exec gosu "${USER}:${GROUP}" /bin/bash
+				exec ${_GOSU} /bin/bash
 			else
 				echo "[INFO]: Executing command -> ${*}"
-				exec gosu "${USER}:${GROUP}" /bin/bash -c "$@" || exit 2
+				exec ${_GOSU} /bin/bash -c "$@" || exit 2
 			fi
 			exit 0;;
 		*)
